88323275a3fd9e964efaf00169108c2dbda27996
paria kamrani <kamraniparia@gmail.com>
2026-02-19 11:29:50 +0330

    update

--- src/assets/components/Scroll.jsx
+++ src/assets/components/Scroll.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+// وقتی میرفتی تو صفحه کیک شکلاتی صفحه اسکرول شده بود و پایین صفحه باز میشد  این کامپوننت اسکرول رو رفرش 
+// میکنه و دیگه صفحه جدید از پایین نمایش داده نمیشه 
+
 
 function Scroll() {
   const { pathname } = useLocation();
